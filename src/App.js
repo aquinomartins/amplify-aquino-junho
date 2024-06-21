70da5add0441f4d2eaa32a2c18a7f9e5da225a16
Fernando Aquino Martins <72260441+aquinomartins@users.noreply.github.com>
2024-06-21 08:47:02 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Jesus Cristo_O Caminho, A Verdade e A Vida</h1>
         <h2>Maria Santíssima</h2>
-        <h3>São José</h3>
+        <h3>São José, valei-me.</h3>
         <h3>Hello from V2</h3>
       </header>
     </div>
